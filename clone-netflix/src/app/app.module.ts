8b6e0876de119deaa27cd6d191fc033b46f5d924
AishJoshi-2002 <anijodha@gmail.com>
2024-12-03 08:12:30 +0530

    Clone using Angular

--- clone-netflix/src/app/app.module.ts
+++ clone-netflix/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { FooterComponent } from './core/components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BrowseComponent } from './pages/browse/browse.component';
+import { BannerComponent } from './core/components/banner/banner.component';
 
 
 @NgModule({
@@ -19,10 +21,12 @@ import { BrowseComponent } from './pages/browse/browse.component';
     FooterComponent,
     HomeComponent,
     LoginComponent,
-    BrowseComponent
+    BrowseComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([]),
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule
