65b3c304de053b7cb59ad3cb2e4d0becddcc1b78
AishJoshi-2002 <aishjoshi2002@gmail.com>
2025-02-13 21:31:25 +0530

    issue resoved_clone netflix

--- clone-netflix/src/app/pages/browse/browse.component.ts
+++ clone-netflix/src/app/pages/browse/browse.component.ts
@@ -25,18 +25,20 @@ export class BrowseComponent {
   
   movies: IVideoContent[] = [];
   tvShows: IVideoContent[] = [];
-  ratedMovies: IVideoContent[] = [];
+  // ratedMovies: IVideoContent[] = [];
   nowPlayingMovies: IVideoContent[] = [];
+  upcomingMovies: IVideoContent[] = [];
   popularMovies: IVideoContent[] = [];
   topRatedMovies: IVideoContent[] = [];
-  upcomingMovies: IVideoContent[] = [];
+  
 
   sources = [ // array of observables
     this.movieService.getMovies(),
     this.movieService.getTvShows(),
-    this.movieService.getRatedMovies(),
+    // this.movieService.getRatedMovies(),
     this.movieService.getNowPlayingMovies(),
     this.movieService.getUpcomingMovies(),
+    this.movieService.getPopularMovies(),
     this.movieService.getTopRated()
   ]
   ngOnInit() {
@@ -45,22 +47,29 @@ export class BrowseComponent {
     //   this.popularMovies = res.results;
     // })
     forkJoin(this.sources) // requires array of observables
-    .pipe(
-      map(([movies, tvShows, ratedMovies, nowPlaying, upcoming, popular, topRated]) => {
-        this.bannerDetail = this.movieService.getBannerDetail(movies.results[1].id);
-        this.bannerVideo = this.movieService.getBannerVideo(movies.results[1].id);
-        return {movies, tvShows, ratedMovies, nowPlaying, upcoming, popular, topRated}
-      })
-    ).subscribe((res:any)=>{
-      this.movies = res.movies.results as IVideoContent[];
-      this.tvShows = res.tvShows.results as IVideoContent[];
-      this.ratedMovies = res.ratedMovies.results as IVideoContent[];
-      this.nowPlayingMovies = res.nowPlaying.results as IVideoContent[];
-      this.upcomingMovies = res.upcoming.results as IVideoContent[];
-      this.popularMovies = res.popular.results as IVideoContent[];
-      this.topRatedMovies = res.topRated.results as IVideoContent[];
-      this.getMovieKey();
-    })
+      .pipe(
+        map(([movies, tvShows, nowPlaying, upcoming, popular, topRated]) => {
+          // map(([movies, tvShows, ratedMovies, nowPlaying, upcoming, popular, topRated]) => {
+          this.bannerDetail = this.movieService.getBannerDetail(
+            movies.results[1].id
+          );
+          this.bannerVideo = this.movieService.getBannerVideo(
+            movies.results[1].id
+          );
+          // return {movies, tvShows, ratedMovies, nowPlaying, upcoming, popular, topRated}
+          return { movies, tvShows, nowPlaying, upcoming, popular, topRated };
+        })
+      )
+      .subscribe((res: any) => {
+        this.movies = res.movies.results as IVideoContent[];
+        this.tvShows = res.tvShows.results as IVideoContent[];
+        // this.ratedMovies = res.ratedMovies.results as IVideoContent[];
+        this.nowPlayingMovies = res.nowPlaying.results as IVideoContent[];
+        this.upcomingMovies = res.upcoming.results as IVideoContent[];
+        this.popularMovies = res.popular.results as IVideoContent[];
+        this.topRatedMovies = res.topRated.results as IVideoContent[];
+        this.getMovieKey();
+      });
   }
 
   getMovieKey() {
